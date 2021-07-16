ccf6f6ead65ea22719873ad536e797b50d83abd8
tashenoff <kail.dead.kd@gmail.com>
2021-07-16 17:32:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+
+    extend: {
+      zIndex: {
+       '60': '60',
+      }
+    },
+    colors: {
+      primary: '#3335BD',
+      secondary: '#47E189',
+      green:'#47E189',
+      black:'#333',
+      white: '#fff',
+      red: '#F3795F'
+    }
+  }
+}
